4b351b66dbac3a7a83ce58c7283cbdc8fc45049d
Tao Ong <taoong@berkeley.edu>
2018-04-12 12:11:24 -0700

    Fixed landmark layout

--- app/src/main/java/com/example/cs160_sp18/prog3/MainActivity.java
+++ app/src/main/java/com/example/cs160_sp18/prog3/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.cs160_sp18.prog3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 /**
  * Created by taoong on 4/6/18.
@@ -13,12 +18,19 @@ public class MainActivity extends Activity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<CardView> mLandmarks = new ArrayList<CardView>();
+
+    // UI elements
+    Toolbar mToolbar;
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_activity);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        setContentView(R.layout.landmark_layout);
+        mRecyclerView = (RecyclerView) findViewById(R.id.landmark_recycler);
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        layout = (RelativeLayout) findViewById(R.id.landmark_layout);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -28,9 +40,17 @@ public class MainActivity extends Activity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        mAdapter = new MainAdapter(myDataset);
+        setAdapterAndUpdateData();
+    }
+
+    private void setAdapterAndUpdateData() {
+        // create a new adapter with the updated mComments array
+        // this will "refresh" our recycler view
+        mAdapter = new MainAdapter(mLandmarks, this);
         mRecyclerView.setAdapter(mAdapter);
+
+        // scroll to the last comment
+        mRecyclerView.smoothScrollToPosition(mLandmarks.size() - 1);
     }
 
 }
